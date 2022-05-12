9da64e830cdc81b3b39a924e71af3d89cc42f321
Juliana <juaiasi@gmail.com>
2022-05-12 09:39:00 -0300

    criando e instanciando uma classe no javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente()
+cliente1.nome = "João"
+cliente1.cpf = "23423423"
+cliente1.agencia = "123"
+cliente1.saldo = 10000
+console.log(cliente1)
